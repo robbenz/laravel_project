ed7b2df8a7b8e22dc5554ea185d8cf08a2d112be
Benzy25 <benz_rob@yahoo.com>
2018-03-04 09:49:48 -0500

    install sass and bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,14 @@
   <head>
     <meta charset="utf-8">
     <title>Acme</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
+    @include('inc.navbar')
     @yield('content')
 
     @include('inc.sidebar')
 
   </body>
+
 </html>
