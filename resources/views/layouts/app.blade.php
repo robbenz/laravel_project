85545196cb51ee8a022368e8fc77d945d3137853
Benzy25 <benz_rob@yahoo.com>
2018-03-07 07:01:26 -0500

    workflow on laravel app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Acme</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/basicwebsite/public/css/app.css">
   </head>
   <body>
     @include('inc.navbar')
